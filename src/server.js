8a241e8dca56900d571e89e6c4abd3a6c79c3cc1
DCoder206 <62135445+DCoder206@users.noreply.github.com>
2024-01-05 16:08:49 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,39 @@
-
+import { networkInterfaces } from "os";
+import express from "express";
+import { createServer } from "http";
+import { Server as socketIO } from "socket.io";
+import { fileURLToPath } from "url";
+import path from "path";
+function IP() {
+    const interfaces = networkInterfaces();
+    for (const name of Object.keys(interfaces)) {
+        for (const net of interfaces[name]) {
+            if (net.family === 'IPv4' && !net.internal) {
+                return net.address;
+            }
+        }
+    }
+}
+const app = express();
+const server = createServer(app);
+const io = new socketIO(server);
+const port = 9000;
+app.use(express.static(path.dirname(fileURLToPath(import.meta.url))));
+io.on("connection",(socket) => {
+    console.log("User connected");
+    socket.on("set username",(username) => {
+        socket.username = username;
+        console.log(`${username} joined`);
+        io.emit("chat message",`<strong>• ${username}</strong> joined`);
+    })
+    socket.on("chat message",(msg) => {
+        io.emit("chat message",`<strong>${socket.username}:</strong> ${msg}`);
+    })
+    socket.on("disconnect",() => {
+        io.emit("chat message",`${socket.username} left`);
+        console.log(`${socket.username} disconnected`);
+    })
+})
+server.listen(port,() => {
+    console.log(`Server running on http://${IP()}:${port}`);
+})
