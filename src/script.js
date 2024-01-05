db3569e974f985fea470f2f7883224c5a37e068c
DCoder206 <62135445+DCoder206@users.noreply.github.com>
2024-01-05 16:08:22 +0530

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,1 +1,39 @@
-
+document.addEventListener("DOMContentLoaded", () => {
+    const nameInput = document.querySelector("#nameInput");
+    const setNameButton = document.querySelector("#setNameButton");
+    nameInput.addEventListener("keyup", (event) => {
+        if (event.key === "Enter") {
+            setNameButton.click();
+        }
+    });
+})
+document.addEventListener("DOMContentLoaded", () => {
+    const messageInput = document.querySelector("#messageInput");
+    const sendMessageButton = document.querySelector("#sendMessageButton");
+    messageInput.addEventListener("keyup", (event) => {
+        if (event.key === "Enter") {
+            sendMessageButton.click();
+        }
+    });
+});
+const socket = io();
+const capitalize = (str) => { return str.substring(0,1).toUpperCase() + str.substring(1).toLowerCase() }
+let userName = "";
+function setUsername() {
+    const nameInput = document.getElementById("nameInput").value;
+    socket.emit("set username",nameInput);
+}
+function sendMessage() {
+    const messageInput = document.getElementById("messageInput").value;
+    socket.emit("chat message",messageInput);
+}
+function showMessage(message) {
+    const messagesDiv = document.getElementById("messages");
+    const p = document.createElement("p");
+    p.textContent = message;
+    messagesDiv.appendChild(p);
+}
+socket.on("chat message",(msg) => {
+    const messagesDiv = document.getElementById("messages");
+    messagesDiv.innerHTML += `<p>${msg}</p>`;
+})
